f60504a289acaee50fcc6d6c9433c15a175e79a2
sksinghvlogger-del <sksinghvlogger@gmail.com>
2025-12-29 09:55:05 +0530

    Update Practical-1.c

--- Practical-1.c
+++ Practical-1.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 int main() {
-    printf("Sneha Mishra,Erp Id-RU-25-113435\n");
+    printf("Shivam Raj,Erp Id-RU-25-11375\n");
     printf("My name is Shivam Raj\n");
     printf("I am 17 years old");
     return 0;
+
 }
